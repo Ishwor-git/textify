be9fda656d8a99eb9ca5d3ecd1bb22905d3cb8b1
Ishwor-git <raishwor2@gmail.com>
2024-02-26 22:13:06 +0545

    basic functions implementation

---
+++ docs/app.js
@@ -0,0 +1,77 @@
+function getSelectedText() {
+  var textarea = document.getElementById("textarea-main");
+
+  if (textarea.selectionStart != undefined) {
+    var startPos = textarea.selectionStart;
+    var endPos = textarea.selectionEnd;
+    var selectedText = textarea.value.substring(startPos, endPos);
+    return selectedText;
+  } else {
+    return 0;
+  }
+}
+
+// Html Components
+var textArea = document.getElementById("textarea-main");
+
+let buttonUpper = document.getElementById("toUpper");
+buttonUpper.addEventListener("click", function () {
+  if (getSelectedText() != 0) {
+    let selectedText = getSelectedText();
+    let text = textArea.value;
+    text = text.replace(selectedText, selectedText.toUpperCase());
+    textArea.value = text;
+  } else {
+    textArea.value = textArea.value.toUpperCase();
+  }
+});
+
+let buttonLower = document.getElementById("toLower");
+buttonLower.addEventListener("click", function () {
+  if (getSelectedText() != 0) {
+    let selectedText = getSelectedText();
+    let text = textArea.value;
+    text = text.replace(selectedText, selectedText.toLowerCase());
+    textArea.value = text;
+  } else {
+    textArea.value = textArea.value.toLowerCase();
+  }
+});
+
+let buttonClear = document.getElementById("clear");
+buttonClear.addEventListener("click", function () {
+  if (getSelectedText() != 0) {
+    let selectedText = getSelectedText();
+    let text = textArea.value;
+    text = text.replace(selectedText, "");
+    textArea.value = text;
+  } else {
+    textArea.value = "";
+  }
+});
+
+let buttonFormat = document.getElementById("formatSpace");
+buttonFormat.addEventListener("click", function () {
+  if (getSelectedText() != 0) {
+    let selectedText = getSelectedText();
+    let text = textArea.value;
+    text = text.replace(selectedText, selectedText.replace(/\s/g, " "));
+    textArea.value = text;
+  } else {
+    textArea.value = textArea.value.replace(/\s/g, " ");
+  }
+});
+
+let buttonCapitalize = document.getElementById("capitalize");
+buttonCapitalize.addEventListener("click", function () {
+  let text = textArea.value;
+  let sentences = text.split(".");
+
+  let capitalizedSentences = sentences.map(function (sentence) {
+    var trimmedSentence = sentence.trim();
+    return trimmedSentence.charAt(0).toUpperCase() + trimmedSentence.slice(1);
+  });
+
+  var resulText = capitalizedSentences.join(". ");
+  textArea.value = resulText;
+});
