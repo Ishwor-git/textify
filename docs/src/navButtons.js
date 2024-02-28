6b9ef1e524515287862ceb22940fac57d890c5bb
Ishwor-git <raishwor2@gmail.com>
2024-02-28 18:02:06 +0545

    navbar added

---
+++ docs/src/navButtons.js
@@ -0,0 +1,12 @@
+let helpButton = document.getElementById("nav-help");
+helpButton.addEventListener("click", function () {
+  console.error("Help function not implemented yet");
+});
+let sourceCodeButton = document.getElementById("nav-source-code");
+sourceCodeButton.addEventListener("click", function () {
+  window.open("https://github.com/Ishwor-git/textify", "_blank");
+});
+let aboutButton = document.getElementById("nav-about-us");
+aboutButton.addEventListener("click", function () {
+  window.open("https://github.com/Ishwor-git", "_blank");
+});
