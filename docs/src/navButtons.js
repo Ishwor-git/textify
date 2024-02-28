cb186ba79b3aa421dbdd963e222d9799db61fdde
Ishwor-git <raishwor2@gmail.com>
2024-02-28 21:03:17 +0545

    height adjustment

--- docs/src/navButtons.js
+++ docs/src/navButtons.js
@@ -1,7 +1,7 @@
-let helpButton = document.getElementById("nav-help");
-helpButton.addEventListener("click", function () {
-  console.error("Help function not implemented yet");
-});
+// let helpButton = document.getElementById("nav-help");
+// helpButton.addEventListener("click", function () {
+//   console.error("Help function not implemented yet");
+// });
 let sourceCodeButton = document.getElementById("nav-source-code");
 sourceCodeButton.addEventListener("click", function () {
   window.open("https://github.com/Ishwor-git/textify", "_blank");
